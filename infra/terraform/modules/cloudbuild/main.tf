e67067fa3ab126a891910ee266d1448976b52719
alex <frankyfix6@gmail.com>
2026-02-20 10:46:23 +0200

    cloud

--- infra/terraform/modules/cloudbuild/main.tf
+++ infra/terraform/modules/cloudbuild/main.tf
@@ -37,6 +37,5 @@ resource "google_cloudbuild_trigger" "app_trigger" {
   substitutions = {
     _REGION     = var.region
     _REPO_NAME  = var.repo_name
-    _CONTAINER_CLUSTER = var.cluster_name
   }
 }
